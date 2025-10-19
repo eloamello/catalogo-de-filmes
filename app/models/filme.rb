517a6a1859d8aa410a7d63651df6734b2ebe58e2
Eloá Mello <eloamello126@gmail.com>
2025-10-19 11:16:36 -0300

    fix: pequeno ajuste de ordenação dos comentários de um filme do mais novo ao mais velho

--- app/models/filme.rb
+++ app/models/filme.rb
@@ -1,5 +1,5 @@
 class Filme < ApplicationRecord
-  has_many :comentarios, dependent: :destroy
+  has_many :comentarios, -> { order(created_at: :desc) }, dependent: :destroy
   belongs_to :usuario
 
   validates :titulo, :sinopse, :ano, :duracao, :diretor, presence: true
