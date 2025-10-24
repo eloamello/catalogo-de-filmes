21914a20073191699f5ee10bc45bdd25074c0edb
Eloá Mello <eloamello126@gmail.com>
2025-10-23 23:30:02 -0300

    refactor: renomeia service para nome mais coerente

--- app/services/importador_filmes_csv.rb
+++ app/services/importador_filmes_csv.rb
@@ -1,16 +1,17 @@
 require "csv"
 
-class ImportadorFilmeCsv
+class ImportadorFilmesCsv
   def self.ler_e_validar(arquivo_attached)
     csv_text = arquivo_attached.download.force_encoding("utf-8")
-
     csv = CSV.parse(csv_text, headers: true, col_sep: ",")
 
-    headers_validos = ["titulo", "sinopse", "ano", "duracao", "diretor"]
+    headers_validos = %w[titulo sinopse ano duracao diretor]
     csv_headers_normalizados = csv.headers.map { |h| h.strip.downcase }
 
     raise "CSV inválido" unless (headers_validos - csv_headers_normalizados).empty?
 
-    csv.map(&:to_h)
+    csv.map do |row|
+      row.to_h.transform_keys { |k| k.strip.downcase }
+    end
   end
 end
