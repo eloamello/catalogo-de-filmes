70adb262cd7fcba83736534abdd96a72497169be
Eloá Mello <eloamello126@gmail.com>
2025-10-25 22:32:25 -0300

    feat: ativa envio de email em prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,12 +53,25 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :solid_queue
   # config.solid_queue.connects_to = { database: { writing: :queue } }
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # Make template changes take effect immediately.
+  config.action_mailer.perform_caching = false
 
-  # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  # Set localhost to be used by links generated in mailer templates.
+  config.action_mailer.default_url_options = { host: "catalogo-de-filmes-710b5b2da3b8.herokuapp.com", protocol: :https }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'eloamello126sec@gmail.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    authentication: :plain,
+    user_name: Rails.application.credentials.dig(:smtp, :user_name),
+    password: Rails.application.credentials.dig(:smtp, :password),
+    domain: 'localhost',
+    enable_starttls_auto: true
+  }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
